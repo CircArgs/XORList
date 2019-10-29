94469db861756516d3afa7182aec32d08624c6e3
CircArgs <35903677+CircArgs@users.noreply.github.com>
2019-10-29 13:30:15 -0400

    XORList now functions. demos

--- src/_XORList/XORList/linked_list.pyx
+++ src/_XORList/XORList/linked_list.pyx
@@ -44,7 +44,7 @@ cdef class CurrentNode(Node):
         return self._prev_ptr
     
     cpdef CurrentNode forward(self):
-        if self.node.prev_xor_next:
+        if self.node.prev_xor_next!=self._prev_ptr:
             return CurrentNode(self.node.prev_xor_next^self._prev_ptr, self._node)
 
     cpdef CurrentNode backward(self):
@@ -58,97 +58,85 @@ cdef class CurrentNode(Node):
 cdef class XORList:
     cdef PyObject* first
     cdef PyObject* last
-    cdef PyObject* current
-
+    cdef int length
+    
+    def __init__(self):
+        self.length=0
     @property
-    def Current(self):
-        return (<CurrentNode> self.current).val
-        
+    def head(self):
+        return (<Node> self.first)
+    
+    @property
+    def tail(self):
+        return (<Node> self.last)
+    
+    
+    
     cpdef append(self, object val):
+        self.length+=1
         #empty list
         if not self.first:
-            t=Node(val)
-            tp=(<PyObject*> t)
-            Py_XINCREF(tp)
-            new=CurrentNode(<uintptr_t> tp)
-            np=<PyObject*> new
-            Py_XINCREF(np)
-            self.first=np
-            self.last=self.first
-            self.current=self.first
+            t=Node(val)#we create our very first node
+            tp=(<PyObject*> t)#we get a pointer to it
+            self.first=tp#finally we set our first node to our new current
+            Py_XINCREF(tp)#+1 ref first
+            self.last=tp#since this is our first node it is our first and last
+            Py_XINCREF(tp)#+1 ref last
+
         #not empty
         else:
-            #one element
-            if self.last==self.first:
-                t=Node(val)
-                tp= (<PyObject*> t)
-                Py_XINCREF(tp)
-                new=CurrentNode(<uintptr_t> tp)
-                np=<PyObject*> new
-                Py_XINCREF(np)
-                self.last=np
-                t=<CurrentNode> self.first
-                prev_xor_next=<uintptr_t> ((<Node> self.last).node^t.prev_ptr)
-                t=t.node
-                t.prev_xor_next=prev_xor_next
-                fp=self.first
-                self.first=<PyObject*> (<Node> self.first).node
-                Py_XDECREF(fp)
-                t=<Node> self.last
-                t.prev_ptr=<uintptr_t> self.first
-            #more than one element
-            else:
-                t=Node(val)
-                tp=<PyObject*> t
-                Py_XINCREF(tp)
-                temp=CurrentNode(<uintptr_t> tp)
-                
-                t=<Node> self.last
-                prev_xor_next=(self.temp.node)^(t.prev_ptr)
-                t=<Node> (<PyObject*> t.node)
-                t.prev_xor_next=prev_xor_next
-                self.temp.prev_ptr=<uintptr_t> self.last
-                self.last=<PyObject*> temp
+            new_node=Node(val, <uintptr_t> self.last)
+            new_ptr=<PyObject*> new_node
+            cur_last=<Node>self.last
+            cur_last.prev_xor_next=cur_last.prev_xor_next^(<uintptr_t> new_ptr)
+            Py_XINCREF(new_ptr)#+1 ref prev_xor_next
+            self.last=new_ptr
+            Py_XINCREF(new_ptr)#+1 ref last
 
     cpdef reverse(self):
         temp=self.last
         self.last=self.first
         self.first=temp
 
-    def __iter__(self):
-        return self
-    
-    def __next__(self):
-        
-        if not self.current or not (<CurrentNode> self.current).forward():
-            t=<uintptr_t> self.first
-            cn=CurrentNode(t)
-            self.current=<PyObject*> cn
-            raise StopIteration()
-        ret = <CurrentNode> self.current
-        fr=ret.forward()
-        self.current=<PyObject*> fr
-        return ret
-
     def __repr__(self):
-        cdef str ret =''
-        for i in self:
-            ret+='<->'+str(i)
-        return ret
+        return str(list(iter_XORList(self)))
+    def __len__(self):
+        return self.length
     
-first=Node(1)
-cdef PyObject* fp=<PyObject*>first
-second=Node(2)
-cdef PyObject* sp=<PyObject*>second
+def iter_XORList(l):
+    head=<PyObject*>l.head
+    cur=CurrentNode(<uintptr_t> head)
+    while cur:
+        yield cur
+        cur=cur.forward()
+        
+    
+# first=Node(1)
+# cdef PyObject* fp=<PyObject*>first
+# second=Node(2)
+# cdef PyObject* sp=<PyObject*>second
 
-third=Node(3)
-first.prev_xor_next=<uintptr_t>sp
-Py_XINCREF(sp)
-del second
+# third=Node(3)
+# first.prev_xor_next=<uintptr_t>sp
+# Py_XINCREF(sp)
+# del second
 
-start=CurrentNode(<uintptr_t> fp)
-print(isinstance(start, Node))
-print(start)
-print(start.forward())
-print(start.forward().backward())
-# print(<Node>sp)
+# start=CurrentNode(<uintptr_t> fp)
+# print(isinstance(start, Node))
+# print(start)
+# print(start.forward())
+# print(start.forward().backward())
+# # print(<Node>sp)
+import time
+
+start=time.time()
+l=XORList()
+for i in range(1000):
+    l.append(i)
+print('time xor ', time.time()-start)
+print(len(l))
+start=time.time()
+l=[]
+for i in range(1000):
+    l.append(i)
+print('time regular ', time.time()-start)
