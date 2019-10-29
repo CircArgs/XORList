d5897685224872c6f218e5de8522ca95ad76ce97
CircArgs <35903677+CircArgs@users.noreply.github.com>
2019-10-29 07:55:27 -0400

    began adding reference counting

--- src/_XORList/XORList/linked_list.pyx
+++ src/_XORList/XORList/linked_list.pyx
@@ -1,3 +1,5 @@
+# %%cython
+
 #TODO: reference counting
 #https://stackoverflow.com/questions/58431732/cython-memory-efficient-doubly-linked-list
 
@@ -6,48 +8,72 @@ from cpython.ref cimport Py_XINCREF, Py_XDECREF
 from libc.stdint cimport uintptr_t
 
 cdef class Node:
-    cdef int prev_xor_next
+    cdef uintptr_t _prev_xor_next
     cdef object val
+    
     def __init__(self, object val, uintptr_t prev_xor_next=0):
-        self.prev_xor_next=prev_xor_next
+        self._prev_xor_next=prev_xor_next
         self.val=val
-
+        
+    @property
+    def prev_xor_next(self):
+        return self._prev_xor_next
+    @prev_xor_next.setter
+    def prev_xor_next(self, uintptr_t p):
+        self._prev_xor_next=p
+    
     def __repr__(self):
         return str(self.val)
 
 
 cdef class CurrentNode(Node):
-    cdef uintptr_t node, prev_ptr
+    cdef uintptr_t _node, _prev_ptr
     def __init__(self, uintptr_t node, uintptr_t prev_ptr=0):
-        self.node = node
-        self.prev_ptr= prev_ptr
-        
+        self._node = node
+        self._prev_ptr= prev_ptr
         
     @property
-    def Node(self):
-        ret=<PyObject *> self.node
+    def val(self):
+        return self.node.val
+    @property
+    def node(self):
+        ret=<PyObject *> self._node
         return <Node> ret
+    @property
+    def prev_ptr(self):
+        return self._prev_ptr
     
     cpdef CurrentNode forward(self):
-        if self.Node.prev_xor_next:
-            return CurrentNode(self.Node.prev_xor_next^self.prev_ptr, self.node)
+        if self.node.prev_xor_next:
+            return CurrentNode(self.node.prev_xor_next^self._prev_ptr, self._node)
 
     cpdef CurrentNode backward(self):
-        if self.prev_ptr:
-            return CurrentNode(self.prev_ptr, (self.node)^(self.prev_ptr).prev_xor_next)
+        if self._prev_ptr:
+            pp=<PyObject*>self._prev_ptr
+            return CurrentNode(self._prev_ptr, self._node^(<Node> pp).prev_xor_next)
+        
+    def __repr__(self):
+        return str(self.node)
 
 cdef class XORList:
     cdef PyObject* first
     cdef PyObject* last
     cdef PyObject* current
 
+    @property
+    def Current(self):
+        return (<CurrentNode> self.current).val
+        
     cpdef append(self, object val):
         #empty list
         if not self.first:
             t=Node(val)
-            tp=<uintptr_t> (<PyObject*> t)
-            new=CurrentNode(tp)
-            self.first=<PyObject*> new
+            tp=(<PyObject*> t)
+            Py_XINCREF(tp)
+            new=CurrentNode(<uintptr_t> tp)
+            np=<PyObject*> new
+            Py_XINCREF(np)
+            self.first=np
             self.last=self.first
             self.current=self.first
         #not empty
@@ -55,21 +81,28 @@ cdef class XORList:
             #one element
             if self.last==self.first:
                 t=Node(val)
-                tp=<uintptr_t> (<PyObject*> t)
-                new=CurrentNode(tp)
-                self.last=<PyObject*> new
-                t=<Node> self.first
-                prev_xor_next=<uintptr_t> (<uintptr_t> (<Node> self.last).node)^(t.prev_ptr)
-                t=<Node> (<PyObject*> t.node)
+                tp= (<PyObject*> t)
+                Py_XINCREF(tp)
+                new=CurrentNode(<uintptr_t> tp)
+                np=<PyObject*> new
+                Py_XINCREF(np)
+                self.last=np
+                t=<CurrentNode> self.first
+                prev_xor_next=<uintptr_t> ((<Node> self.last).node^t.prev_ptr)
+                t=t.node
                 t.prev_xor_next=prev_xor_next
-                self.first=<PyObject*> ((<Node> self.first).node)
+                fp=self.first
+                self.first=<PyObject*> (<Node> self.first).node
+                Py_XDECREF(fp)
                 t=<Node> self.last
                 t.prev_ptr=<uintptr_t> self.first
             #more than one element
             else:
                 t=Node(val)
-                tp=<uintptr_t> (<PyObject*> t)
-                temp=CurrentNode(tp)
+                tp=<PyObject*> t
+                Py_XINCREF(tp)
+                temp=CurrentNode(<uintptr_t> tp)
+                
                 t=<Node> self.last
                 prev_xor_next=(self.temp.node)^(t.prev_ptr)
                 t=<Node> (<PyObject*> t.node)
@@ -103,6 +136,19 @@ cdef class XORList:
             ret+='<->'+str(i)
         return ret
     
-l=XORList()
-l.append(1)
-print(l)
+first=Node(1)
+cdef PyObject* fp=<PyObject*>first
+second=Node(2)
+cdef PyObject* sp=<PyObject*>second
+
+third=Node(3)
+first.prev_xor_next=<uintptr_t>sp
+Py_XINCREF(sp)
+del second
+
+start=CurrentNode(<uintptr_t> fp)
+print(isinstance(start, Node))
+print(start)
+print(start.forward())
+print(start.forward().backward())
+# print(<Node>sp)
